c490ecffecaed57f84af73b6b73d53060ffd2f05
Antrax2680 <160753274+Antrax2680@users.noreply.github.com>
2024-04-04 21:52:26 -0600

    aaaaaaaa
    AFK

---
+++ tarea1-4-de-Abril.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int control = 1;
+	
+	do {
+		cout << "Hola, bienvenido,\n";
+		cout << endl << "¿Quieres seguir en el menú?\n 1) SI\n2) NO";
+		cin >> control;
+	} while (control == 1);
+	cout << "Salio del menú, fin del proceso";
+	return 0;
+}
+
