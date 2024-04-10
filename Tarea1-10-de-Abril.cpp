6901489f10515536b9fd6ca0c606caf0a5b2afa7
Antrax2680 <160753274+Antrax2680@users.noreply.github.com>
2024-04-10 09:44:12 -0600

    ta bien 👍🏻

---
+++ Tarea1-10-de-Abril.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	string pal1;
+	string pal2;
+	cout << "Ingrese dos palabras para unirlas en una sola.\n";
+	cout << "Ingrese la primera palabra: " << endl; 
+	cin >> pal1;
+	cout << "Ingrese la segunda palabra: " << endl;
+	cin >> pal2;
+	if (pal1 , pal2 , '\n');
+	cout << "Las dos palabras unidas serían:\n" << pal1 << pal2 << endl;
+	return 0;
+}
+
