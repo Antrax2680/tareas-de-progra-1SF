d71e6f1a3d8ce10ee120ee08623a351fec91dcca
Antrax2680 <160753274+Antrax2680@users.noreply.github.com>
2024-05-16 23:41:28 -0600

    Add files via upload

---
+++ tarea01-16-de-mayo.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int cifrar(int contra, int numero) {
+	int resultado = contra + numero; 
+	resultado *= numero; 
+	return resultado;
+}
+
+int main() {
+	int contra, numero, contra2;
+	
+
+	cout << "Ingrese una contraseña de números: ";
+	cin >> contra;
+	
+
+	cout << "Ingrese un número para las operaciones: ";
+	cin >> numero;
+	
+
+	contra2 = cifrar(contra, numero);
+	
+	
+	cout << endl << "Contraseña cifrada: " << contra2 << endl;
+	
+	return 0;
+}
+
---
+++ tarea02-16-de-mayo.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string cifrar(const string& cadena) {
+	string resu = cadena;
+	int longi = resu.length();
+	
+	for (int i = 0; i < longi; i++) {
+		char num1 = resu[i];
+		switch (num1) {
+		case 'i':
+		case 'I':
+			resu[i] = '1';
+			break;
+		case 'e':
+		case 'E':
+			resu[i] = '3';
+			break;
+		case 'a':
+		case 'A':
+			resu[i] = '4';
+			break;
+		case 'o':
+		case 'O':
+			resu[i] = '0';
+			break;
+		case 'u':
+		case 'U':
+			resu[i] = '*';
+			break;
+		default:
+			break;
+		}
+	}
+	
+	return resu;
+}
+
+int main() {
+	string frase1, frase2;
+
+	cout << "Ingrese una palabra o frase para cifrarla: ";
+	getline(cin, frase1);
+	
+	frase2 = cifrar(frase1);
+	
+	cout << endl << "Frase cifrada: " << frase2 << endl;
+	
+	return 0;
+}
+
